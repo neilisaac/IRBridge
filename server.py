37f65bfcad39739ae863bf0a3185a2721cd7215f
Neil Isaac <isaac.neil@gmail.com>
2017-03-25 16:57:36 -0400

    added ifttt integration and repeated volume presses

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2
 
+import argparse
 import logging
+import requests
 import serial
 import sys
 import tornado.log
@@ -27,21 +29,47 @@ class SendHandler(tornado.web.RequestHandler):
         self.logger = logging.getLogger(self.__class__.__name__)
         self.serial = serial.Serial(device, baud)
 
-    def post(self, code):
-        self.logger.info("sending {}".format(code))
-        self.serial.write('{}\n'.format(code))
+    def post(self, code, count=1):
+        self.logger.info("sending {} x{}".format(code, count))
+        for _ in range(int(count)):
+            self.serial.write('{}\n'.format(code))
         self.write("sent " + code)
 
 
+class IFTTTHandler(tornado.web.RequestHandler):
+
+    def initialize(self, apikey):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.apikey = apikey
+
+    def post(self, event):
+        self.logger.info("sending {}".format(event))
+        if self.apikey:
+            requests.post('https://maker.ifttt.com/trigger/{}/with/key/{}'.format(event, self.apikey))
+            self.write("triggered " + event)
+        else:
+            self.write("ignored event because api key was not provided " + event)
+
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--apikey', default=None)
+    parser.add_argument('--device', default='/dev/ttyACM0')
+    parser.add_argument('--baud', default=9600)
+    parser.add_argument('--port', default=80)
+    args = parser.parse_args()
+
     tornado.log.enable_pretty_logging()
     app = tornado.web.Application([
         (r"/", FileHandler, dict(filename='remote.html')),
         (r"/icon.png", FileHandler, dict(filename='icon.png', content_type='image/png')),
         (r"/favicon.png", FileHandler, dict(filename='favicon.png', content_type='image/png')),
-        (r"/send/(.*)", SendHandler, dict(device='/dev/ttyACM0', baud=9600)),
+        (r"/send/(.*)/(.*)", SendHandler, dict(device=args.device, baud=args.baud)),
+        (r"/send/(.*)", SendHandler, dict(device=args.device, baud=args.baud)),
+        (r"/ifttt/(.*)", IFTTTHandler, dict(apikey=args.apikey)),
     ])
-    app.listen(80)
+
+    app.listen(args.port)
     tornado.ioloop.IOLoop.current().start()
 
 

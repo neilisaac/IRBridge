44522e4d31d98a409c969502c055d5699c91f329
neilisaac <isaac.neil@gmail.com>
2018-09-03 22:25:52 -0400

    added support for a few voice commands

--- client.go
+++ client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -19,6 +20,7 @@ func runClient(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
+	logger.Info("connecting to command websocket", "addr", addr)
 	conn, _, err := websocket.DefaultDialer.Dial(addr, http.Header{})
 	if err != nil {
 		return err
@@ -45,13 +47,28 @@ func runClient(cmd *cobra.Command, args []string) error {
 				close(commands)
 				break
 			}
+			switch strings.ToLower(c.Code) {
+			case "select tv":
+				c.Code = "dac-opt1"
+			case "select chromecast":
+				c.Code = "dac-opt2"
+			case "select usb":
+				c.Code = "dac-usb"
+			case "turn up the volume":
+				c.Code = "dac-vol-up"
+				c.Repeat = 10
+			case "turn down the volume":
+				c.Code = "dac-vol-down"
+				c.Repeat = 10
+			}
 			commands <- c
 		}
 	}()
 
+	logger.Info("opening serial device", "device", device)
 	dev, err := serial.OpenPort(&serial.Config{Name: device, Baud: 9600})
 	if err != nil {
-		// return err
+		return err
 	}
 	defer dev.Close()
 
@@ -59,7 +76,7 @@ func runClient(cmd *cobra.Command, args []string) error {
 		if c.Repeat == 0 {
 			c.Repeat = 1
 		}
-		logger.Info("sending command", "code", c.Code, "times", c.Repeat)
+		logger.Info("sending command to dac", "code", c.Code, "times", c.Repeat)
 		for i := 0; i < c.Repeat; i++ {
 			if i > 0 {
 				time.Sleep(10 * time.Millisecond)
